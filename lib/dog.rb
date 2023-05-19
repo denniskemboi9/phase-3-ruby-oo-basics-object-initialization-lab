86f0a680d1ecc8d23b57155fda0ebf8360ffcfc9
DennisKemboi <cherogonydennis9@gmail.com>
2023-05-20 01:05:31 +0300

    Completed Assignment

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+# require 'pry'
+
+class Dog
+    def initialize(name, breed="Mutt")
+        @name = name
+        @breed = breed
+    end
+end
+
+# puts d1 = Dog.new
+# # puts d1.name("Kevo")
+
+# binding.pry
---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person
+    attr_accessor(:name)
+    def initialize(name)
+        @name = name
+    end
+end
+
+# p1 = Person.new
+# p1.name "Kemboi"
+# puts p1
